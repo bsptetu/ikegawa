6325c131bf50ca9752ad2b1bfbefb744ed172ef0
bsptetu <55188675+bsptetu@users.noreply.github.com>
2021-01-07 21:47:05 +0900

    20210107

--- js/plugins.js
+++ js/plugins.js
@@ -7,6 +7,7 @@ var $plugins =
 {"name":"OnlineAvatar","status":true,"description":"Firebaseを使ってプレイヤーをオンライン同期します。","parameters":{"apiKey":"AIzaSyA283i_MIkhDyHwPy1aKo-ZIQ6sgQJ-3-0","authDomain":"ikegawasyoukai.firebaseapp.com","databaseURL":"https://ikegawasyoukai-default-rtdb.firebaseio.com","avatarEvent":"0","syncSwitchStart":"0","syncSwitchEnd":"0","syncVariableStart":"100","syncVariableEnd":"5000"}},
 {"name":"PictureCallCommon","status":true,"description":"ピクチャのボタン化プラグイン","parameters":{"透明色を考慮":"true","ピクチャ番号の変数番号":"0","ポインタX座標の変数番号":"0","ポインタY座標の変数番号":"0","タッチ操作抑制":"false","戦闘中常にコモン実行":"false","無効スイッチ":"0"}},
 {"name":"DTextPicture","status":true,"description":"動的文字列ピクチャ生成プラグイン","parameters":{}},
-{"name":"111_InputForm","status":true,"description":"ゲーム画面上にHTMLの入力フォームを表示します","parameters":{"OK Button Text":"決定","Display Cancel Button":"1","Cancel Button Text":"キャンセル","Judge Switch Interval":"100","Switch ID Selected OK":"0","Force OK Switch ID":"0","Force Cancel Switch ID":"0"}},
-{"name":"PictureVariableSetting","status":true,"description":"ピクチャ関連のイベント機能拡張プラグイン","parameters":{"初期値":"OFF","ピクチャ表示最大数":"120"}}
+{"name":"111_InputForm","status":true,"description":"ゲーム画面上にHTMLの入力フォームを表示します","parameters":{"OK Button Text":"OK","Display Cancel Button":"1","Cancel Button Text":"Cancel","Judge Switch Interval":"100","Switch ID Selected OK":"0","Force OK Switch ID":"0","Force Cancel Switch ID":"0"}},
+{"name":"PictureVariableSetting","status":true,"description":"ピクチャ関連のイベント機能拡張プラグイン","parameters":{"初期値":"OFF","ピクチャ表示最大数":"120"}},
+{"name":"111_InputForm2","status":true,"description":"フォーム作って文字入力（修正版）","parameters":{}}
 ];
